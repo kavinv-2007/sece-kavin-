3435f7e98cdc441753f2cddbc90c131701f87529
kavinv-2007 <kavin.v2025ece@sece.ac.in>
2026-09-16 22:20:53 +0530

    Fix hello endpoint configuration

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -1,4 +1,5 @@
-package com.sece.eceb.Controller;
+
+package com.example.demo.Controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
